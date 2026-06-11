3c07e7a6daf62f97cfe9293ed901765cf9a236f7
Hugo Hsi <hugohsidev@gmail.com>
2026-06-11 13:19:00 -0400

    Validate remote document inputs with valibot

--- demo/src/routes/admin/documents.remote.ts
+++ demo/src/routes/admin/documents.remote.ts
@@ -1,5 +1,6 @@
 import { command, query } from '$app/server';
 import { error } from '@sveltejs/kit';
+import * as v from 'valibot';
 
 import { stone, type DocumentData } from '$lib/server/cms/stone';
 
@@ -21,46 +22,51 @@ type UpdateInput = MutationInput & {
 	id: string;
 };
 
+const collectionSchema = v.object({
+	collection: v.string()
+});
+
+const findByIdSchema = v.object({
+	collection: v.string(),
+	id: v.string()
+});
+
+const mutationSchema = v.object({
+	collection: v.string(),
+	data: v.record(v.string(), v.string())
+});
+
+const updateSchema = v.object({
+	collection: v.string(),
+	id: v.string(),
+	data: v.record(v.string(), v.string())
+});
+
 function requireCollection(collection: CollectionName) {
 	if (!stone.getCollection(collection)) error(400, 'Unsupported collection');
 }
 
-export const listDocuments = query<CollectionInput, Awaited<ReturnType<typeof stone.find>>>(
-	'unchecked',
-	async (input) => {
-		requireCollection(input.collection);
-		return stone.find(input);
-	}
-);
+export const listDocuments = query(collectionSchema, async (input) => {
+	requireCollection(input.collection);
+	return stone.find(input);
+});
 
-export const getDocument = query<FindByIdInput, Awaited<ReturnType<typeof stone.findById>>>(
-	'unchecked',
-	async (input) => {
-		requireCollection(input.collection);
-		return stone.findById(input);
-	}
-);
+export const getDocument = query(findByIdSchema, async (input) => {
+	requireCollection(input.collection);
+	return stone.findById(input);
+});
 
-export const createDocument = command<MutationInput, Awaited<ReturnType<typeof stone.create>>>(
-	'unchecked',
-	async (input) => {
-		requireCollection(input.collection);
-		return stone.create(input);
-	}
-);
+export const createDocument = command(mutationSchema, async (input) => {
+	requireCollection(input.collection);
+	return stone.create(input);
+});
 
-export const updateDocument = command<UpdateInput, Awaited<ReturnType<typeof stone.update>>>(
-	'unchecked',
-	async (input) => {
-		requireCollection(input.collection);
-		return stone.update(input);
-	}
-);
+export const updateDocument = command(updateSchema, async (input) => {
+	requireCollection(input.collection);
+	return stone.update(input);
+});
 
-export const deleteDocument = command<FindByIdInput, Awaited<ReturnType<typeof stone.delete>>>(
-	'unchecked',
-	async (input) => {
-		requireCollection(input.collection);
-		return stone.delete(input);
-	}
-);
+export const deleteDocument = command(findByIdSchema, async (input) => {
+	requireCollection(input.collection);
+	return stone.delete(input);
+});
